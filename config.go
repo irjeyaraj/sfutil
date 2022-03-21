d57b337a704bd86a615cfc04232cd79604226313
Immanuel Raja Jeyaraj <irj@sefier.com>
2022-03-21 15:00:12 +0530

    toml config to viper config.

---
+++ config.go
@@ -0,0 +1,33 @@
+/*
+ * util_config.go
+ *
+ * Copyright 2022 Immanuel Jeyaraj
+ *
+ * Author: Immanuel Jeyaraj <irj@sefier.com>
+ *
+ * Created date: 3 June 2019
+ */
+
+package sfutil
+
+import (
+	"github.com/spf13/viper"
+)
+
+var ConfigData map[string]interface{}
+
+func ReadConfig(confFile string, confType string, appName string) (map[string]interface{}, error) {
+	viper.SetConfigName(confFile)
+	viper.SetConfigType(confType)
+	viper.AddConfigPath("/etc/" + appName + "/")
+	viper.AddConfigPath("$HOME/." + appName + "/")
+	viper.AddConfigPath("./etc")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	ConfigData = viper.AllSettings()
+	return ConfigData, err
+}
+
+func GetConfigValue(key string) string {
+	return ConfigData(key)
+}
